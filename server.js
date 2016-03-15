c0a5ec2cb1c568b633bbebe299f2c306e962baa3
Kevin Moore <kevin391@msn.com>
2016-03-15 12:47:08 +0000

    initialize rebo

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+//var parseString = require('xml2js').parseString;
+var app = express();
+var PORT = 3000;
+var middleware = require('./middleware.js')
+
+
+//app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res){
+	res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function() {
+	console.log('Express Server Started on port ' + PORT + " :) ");
+});
